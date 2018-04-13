d8785d221d382b7513b22607f9864829d4ae98c9
Telma Gama <telmagamasilva@gmail.com>
2018-04-13 08:28:57 -0300

    angular

---
+++ app.ts
@@ -0,0 +1,10 @@
+let mensagem: string = "Seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Banana']
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Banana']
+
+let notaDasProvas: Array<number> = [7.5, 8,9]
+let notaDasProvas2: number[] = [7.5, 8,9]
+
+
---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var mensagem = "Seja bem vindo!!!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ['Uva', 'Banana', 'Banana'];
+var listaDeFrutas2 = ['Uva', 'Banana', 'Banana'];
+var notaDasProvas = [7.5, 8, 9];
+var notaDasProvas2 = [7.5, 8, 9];
